432a95d886bb115b8b4ed77d43376c40fe792eba
Alexey Onoprienko <oleksiyonoprienko@gmail.com>
2021-03-05 18:15:35 +0200

    Apply search bar and searching possibility

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -14,6 +14,7 @@ class PlaceModel: Object {
     @objc dynamic var country: String?
     @objc dynamic var city: String?
     @objc dynamic var imageData: Data?
+    @objc dynamic var date = Date()
     
     
     convenience init(name: String, country: String?, city: String?, imageData: Data?) {
--- MyPlaces/StorageManager.swift
+++ MyPlaces/StorageManager.swift
@@ -7,27 +7,20 @@
 
 import RealmSwift
 
-
 let realm = try! Realm()
 
 class StorageManager {
     
-    
     static func saveObject(_ place: PlaceModel) {
-        
-        
         try! realm.write {
             realm.add(place)
         }
-        
     }
     
     static func deleteObject(_ place: PlaceModel) {
-        
         try! realm.write {
             realm.delete(place)
         }
-        
     }
     
 }
